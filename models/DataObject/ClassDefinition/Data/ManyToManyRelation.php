6ed6b1a1269e7128572bf13f1ea15d6276965745
JiaJia Ji <kingjia90@gmail.com>
2025-05-09 08:16:40 +0200

    [Bug, EC]: Follow-up to #17814, add more brickprefixes to avoid ambiguity (#18391)
    * workaround for ambiguity in different bricks
    
    * Apply php-cs-fixer changes
    
    ---------
    
    Co-authored-by: kingjia90 <6014195+kingjia90@users.noreply.github.com>

--- models/DataObject/ClassDefinition/Data/ManyToManyRelation.php
+++ models/DataObject/ClassDefinition/Data/ManyToManyRelation.php
@@ -790,9 +790,16 @@ class ManyToManyRelation extends AbstractRelations implements QueryResourcePersi
      */
     public function getFilterConditionExt(mixed $value, string $operator, array $params = []): string
     {
+        $prefix = '';
         $name = $params['name'] ?: $this->name;
 
-        return $this->getRelationFilterCondition($value, $operator, $name);
+        if ($params['brickPrefix']) {
+            // The brick prefix is always quoted and with a dot suffix, so removing the first
+            // and second last character to unquote
+            $prefix = substr($params['brickPrefix'], 1, -2) . substr($params['brickPrefix'], -1);
+        }
+
+        return $this->getRelationFilterCondition($value, $operator, $prefix . $name);
     }
 
     public function getQueryColumnType(): string
