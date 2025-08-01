bfdd08d7046961c83479a595b42f9b36d240e74e
herbertroth <126679157+herbertroth@users.noreply.github.com>
2025-08-01 09:38:07 +0000

    Apply php-cs-fixer changes

--- tests/Model/Document/Editable/ImageTest.php
+++ tests/Model/Document/Editable/ImageTest.php
@@ -28,6 +28,7 @@ use Pimcore\Tests\Support\Util\TestHelper;
 class ImageTest extends ModelTestCase
 {
     protected ?Page $testPage = null;
+
     protected ?Asset\Image $testAsset = null;
 
     protected function setUp(): void
@@ -41,13 +42,13 @@ class ImageTest extends ModelTestCase
     {
         $image = new Image();
         $image->setName('test-image');
-        
+
         // Test initial empty state
         $this->assertTrue($image->isEmpty());
         $this->assertNull($image->getImage());
         $this->assertNull($image->getId());
         $this->assertEquals('', $image->getSrc());
-        
+
         // Test setting image
         $image->setImage($this->testAsset);
         $this->assertFalse($image->isEmpty());
@@ -60,11 +61,11 @@ class ImageTest extends ModelTestCase
     {
         $image = new Image();
         $image->setName('test-image');
-        
+
         // Test default empty alt text
         $this->assertEquals('', $image->getAlt());
         $this->assertEquals('', $image->getText());
-        
+
         // Test setting alt text
         $altText = 'Test alt text';
         $image->setText($altText);
@@ -76,21 +77,21 @@ class ImageTest extends ModelTestCase
     {
         $image = new Image();
         $image->setName('test-image');
-        
+
         // Test default cropping values
         $this->assertFalse($image->getCropPercent());
         $this->assertEquals(0.0, $image->getCropWidth());
         $this->assertEquals(0.0, $image->getCropHeight());
         $this->assertEquals(0.0, $image->getCropTop());
         $this->assertEquals(0.0, $image->getCropLeft());
-        
+
         // Test setting cropping values
         $image->setCropPercent(true);
         $image->setCropWidth(50.5);
         $image->setCropHeight(75.3);
         $image->setCropTop(10.2);
         $image->setCropLeft(20.7);
-        
+
         $this->assertTrue($image->getCropPercent());
         $this->assertEquals(50.5, $image->getCropWidth());
         $this->assertEquals(75.3, $image->getCropHeight());
@@ -102,18 +103,18 @@ class ImageTest extends ModelTestCase
     {
         $image = new Image();
         $image->setName('test-image');
-        
+
         // Test default empty arrays
         $this->assertEquals([], $image->getHotspots());
         $this->assertEquals([], $image->getMarker());
-        
+
         // Test setting hotspots and markers
         $hotspots = [['x' => 10, 'y' => 20, 'width' => 30, 'height' => 40]];
         $markers = [['x' => 50, 'y' => 60]];
-        
+
         $image->setHotspots($hotspots);
         $image->setMarker($markers);
-        
+
         $this->assertEquals($hotspots, $image->getHotspots());
         $this->assertEquals($markers, $image->getMarker());
     }
@@ -127,9 +128,9 @@ class ImageTest extends ModelTestCase
         $image->setCropPercent(true);
         $image->setCropWidth(50.0);
         $image->setCropHeight(75.0);
-        
+
         $data = $image->getData();
-        
+
         $this->assertIsArray($data);
         $this->assertEquals($this->testAsset->getId(), $data['id']);
         $this->assertEquals('Test alt text', $data['alt']);
@@ -144,7 +145,7 @@ class ImageTest extends ModelTestCase
     {
         $image = new Image();
         $image->setName('test-image');
-        
+
         $editmodeData = [
             'id' => $this->testAsset->getId(),
             'alt' => 'Editmode alt text',
@@ -156,9 +157,9 @@ class ImageTest extends ModelTestCase
             'hotspots' => [],
             'marker' => [],
         ];
-        
+
         $image->setDataFromEditmode($editmodeData);
-        
+
         $this->assertEquals($this->testAsset->getId(), $image->getId());
         $this->assertEquals('Editmode alt text', $image->getAlt());
         $this->assertTrue($image->getCropPercent());
@@ -172,10 +173,10 @@ class ImageTest extends ModelTestCase
     {
         $image = new Image();
         $image->setName('test-image');
-        
+
         $descriptor = new ElementDescriptor('asset', $this->testAsset->getId());
         $image->setImage($descriptor);
-        
+
         // When getting the image, it should resolve to the actual asset
         $resolvedImage = $image->getImage();
         $this->assertInstanceOf(Asset\Image::class, $resolvedImage);
@@ -187,15 +188,15 @@ class ImageTest extends ModelTestCase
         $image = new Image();
         $image->setName('test-image');
         $image->setImage($this->testAsset);
-        
+
         // Test thumbnail configuration through editmode data
         $editmodeData = [
             'id' => $this->testAsset->getId(),
-            'thumbnail' => 'test-thumbnail'
+            'thumbnail' => 'test-thumbnail',
         ];
-        
+
         $image->setDataFromEditmode($editmodeData);
-        
+
         $this->assertEquals('test-thumbnail', $image->getThumbnailConfig());
     }
 
@@ -207,16 +208,16 @@ class ImageTest extends ModelTestCase
         $image->setHotspots([['data' => []]]);
         $image->setMarker([['data' => []]]);
         $image->setCropPercent(true);
-        
+
         $oldId = $this->testAsset->getId();
         $newId = $oldId + 1000;
-        
+
         $idMapping = [
-            'asset' => [$oldId => $newId]
+            'asset' => [$oldId => $newId],
         ];
-        
+
         $image->rewriteIds($idMapping);
-        
+
         // After rewriting, ID should be updated and crop/hotspot data reset
         $this->assertEquals($newId, $image->getId());
         $this->assertEquals([], $image->getHotspots());
@@ -232,9 +233,9 @@ class ImageTest extends ModelTestCase
         $image = new Image();
         $image->setName('test-image');
         $image->setImage($this->testAsset);
-        
+
         $tags = $image->getCacheTags($this->testPage);
-        
+
         $this->assertIsArray($tags);
         $expectedTag = $this->testAsset->getCacheTag();
         $this->assertArrayHasKey($expectedTag, $tags);
@@ -245,9 +246,9 @@ class ImageTest extends ModelTestCase
         $image = new Image();
         $image->setName('test-image');
         $image->setImage($this->testAsset);
-        
+
         $dependencies = $image->resolveDependencies();
-        
+
         $this->assertIsArray($dependencies);
         $expectedKey = 'asset_' . $this->testAsset->getId();
         $this->assertArrayHasKey($expectedKey, $dependencies);
@@ -266,9 +267,9 @@ class ImageTest extends ModelTestCase
         $image = new Image();
         $image->setName('test-image');
         $image->setImage($this->testAsset);
-        
+
         $sleepVars = $image->__sleep();
-        
+
         // The 'image' property should be excluded from serialization
         $this->assertIsArray($sleepVars);
         $this->assertNotContains('image', $sleepVars);
