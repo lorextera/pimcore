4fb15b23ea6472134ee9964d9a16940bc1d41d25
Blackbit <jan.walther@blackbit.de>
2025-08-08 12:22:27 +0200

    [Search / Object selector] Sort class names + Search in whole class name (not only from beginning) (#17924)
    * sort class names in search / object selector
    
    * match class name anywhere, not only from beginning

--- bundles/SimpleBackendSearchBundle/public/js/pimcore/element/selector/object.js
+++ bundles/SimpleBackendSearchBundle/public/js/pimcore/element/selector/object.js
@@ -138,6 +138,8 @@ pimcore.bundle.search.element.selector.object = Class.create(pimcore.bundle.sear
             }
         }
 
+        filterClassStore.sort((class1, class2) => class1[1].localeCompare(class2[1]));
+
         var selectedClassValue = selectedClassStore.join(",");
         if(filterClassStore.length > 1) {
             filterClassStore.splice(0,0,[selectedClassValue, t("all_types")]);
@@ -152,6 +154,7 @@ pimcore.bundle.search.element.selector.object = Class.create(pimcore.bundle.sear
             typeAhead: true,
             forceSelection: true,
             selectOnFocus: true,
+            anyMatch: true,
             value: selectedClassValue,
             listeners: {
                 select: this.changeClass.bind(this)
